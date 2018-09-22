60083fb1a561cdfc02508ca290086739f30331b5
Kaleb Juliu <kalebjuliu@gmail.com>
2018-09-22 09:42:33 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React App</h1>
+        <p>Welcome</p>
+        <Ninjas />
+      </div>
+    );
+  }
+}
+
+export default App;
